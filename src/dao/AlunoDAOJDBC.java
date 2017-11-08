7f687adbfd0c9918216078e1b3aa9279451c6b2e
Leandro Cesar de Almeida <llmanutencao@bol.com.br>
2017-11-08 20:33:24 -0200

    Update methodo consultar login de aluno Classe AlunoDAOJDBC

--- src/dao/AlunoDAOJDBC.java
+++ src/dao/AlunoDAOJDBC.java
@@ -6,7 +6,10 @@
 package dao;
 
 import java.awt.HeadlessException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import javax.swing.JOptionPane;
 import modelo.Aluno;
 import tools.DAOBaseJDBC;
@@ -18,14 +21,33 @@ import tools.DAOBaseJDBC;
 public class AlunoDAOJDBC extends DAOBaseJDBC implements AlunoDAO {
     
     @Override
-    public Aluno consultarLogin(String user, String password){
+    public Aluno consultarLogin(String loginUser, String loginPass){
         
         Aluno alunoLido = null;
-        
+        String consulta = 
+        "SELECT idAaluno, nome, matricula, nascimento, email, telefone FROM Aluno WHERE user = ? AND password = ?";
         try{
-            JOptionPane.showMessageDialog(null, "######");
+            PreparedStatement stmt = conn.prepareStatement(consulta);
+            stmt.setString(1, loginUser);
+            stmt.setString(2, loginPass);
+            ResultSet resultado = stmt.executeQuery();
             
-        }catch(HeadlessException e){
+            if(resultado.next()){
+                alunoLido = new Aluno();
+                alunoLido.setIdAluno(resultado.getInt("idAluno"));
+                alunoLido.setNome(resultado.getString("nome"));
+                alunoLido.setMatricula(resultado.getInt("matricula"));
+                SimpleDateFormat fmt = new SimpleDateFormat("dd/MM/yyyy");
+                alunoLido.setNascimento(resultado.getString(fmt.format("nascimento")));
+                alunoLido.setEmail(resultado.getString("email"));
+                alunoLido.setTelefone(resultado.getString("telefone"));
+                stmt.close();
+                
+            }else{
+                JOptionPane.showMessageDialog(null, "Usuário Não encontrado");
+                return null;
+            }
+        }catch(SQLException e){
             
             JOptionPane.showMessageDialog(null, "Erro no banco de dados: " + e.getMessage());
         }
