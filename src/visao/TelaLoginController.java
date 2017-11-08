4b4604cb97d73524a55c5ceaf6d9eb13d2db26dc
Leandro Cesar de Almeida <llmanutencao@bol.com.br>
2017-11-08 21:06:55 -0200

    Criando a navegação para outra tela (Aluno)

--- src/visao/TelaLoginController.java
+++ src/visao/TelaLoginController.java
@@ -5,10 +5,20 @@
  */
 package visao;
 
+import dao.AlunoDAOJDBC;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javax.swing.JOptionPane;
 
 /**
  * FXML Controller class
@@ -17,12 +27,47 @@ import javafx.stage.StageStyle;
  */
 public class TelaLoginController implements Initializable {
 
+    @FXML
+    private TextField txtUser;
+    @FXML
+    private Button btnLogar;
+    @FXML
+    private PasswordField txtPass;
+
     /**
      * Initializes the controller class.
+     * @param url
+     * @param rb
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        
+        btnLogar.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                
+                AlunoDAOJDBC alunoDAO = new AlunoDAOJDBC();
+                
+                if(alunoDAO.consultarLogin(txtUser.getText(), txtPass.getText()) == null){
+                    
+                    JOptionPane.showMessageDialog(null, "Usuário Não encontrado");
+                    
+                }else{
+                    
+                    Stage stage  new Stage();
+                    Parent root = null;
+                    
+                }
+            }
+        });{
+        
+        
+        
+    }
+        
+        
+        
     }
        
     
