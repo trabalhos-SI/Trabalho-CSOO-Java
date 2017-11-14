1c40f8e26574ba5f76ec1805cd20b73db41f54cd
Leandro Cesar de Almeida <llmanutencao@bol.com.br>
2017-11-14 00:19:40 -0200

    Classe endereco

---
+++ src/modelo/Endereco.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author Leandro
+ */
+public class Endereco {
+    
+    private int idEndereco;
+    private String rua;
+    private String bairro;
+    private String cidade;
+    private String estado;
+
+    public Endereco() {
+    }
+
+    public Endereco(int idEndereco) {
+        this.idEndereco = idEndereco;
+    }
+
+    public int getIdEndereco() {
+        return idEndereco;
+    }
+
+    public void setIdEndereco(int idEndereco) {
+        this.idEndereco = idEndereco;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
+    
+    
+}
