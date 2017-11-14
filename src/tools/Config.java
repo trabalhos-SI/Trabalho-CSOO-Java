3f546550bff0e883e66b225a94eca9beaa3d2c6f
Leandro Cesar de Almeida <llmanutencao@bol.com.br>
2017-11-13 23:59:52 -0200

    Correção nome banco

--- src/tools/Config.java
+++ src/tools/Config.java
@@ -13,7 +13,7 @@ package tools;
 public interface Config {
     
     public static final String NOME_DRIVER = "com.mysql.jdbc.Driver";
-    public static final String BD_URL = "jdbc:mysql://localhost:3306/trabalho"; // #DEFINIR A BASE DE DADOS E MODIFICAR
+    public static final String BD_URL = "jdbc:mysql://localhost:3306/mydb"; // #DEFINIR A BASE DE DADOS E MODIFICAR
     public static final String BD_LOGIN = "root";
     public static final String BD_SENHA = "";
 }
